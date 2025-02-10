718940688df98401d0300990ee895bb439ad8c52
lightpc <afoageektech@gmail.com>
2025-02-10 08:32:00 +0100

    `Refactor DepartmentCat component:
    - Changed button z-index to 1000
    - Modified initial and exit animation positions for the links dropdown
    - Changed z-index of the links dropdown to 100

--- src/components/hero/DepartmentCat.jsx
+++ src/components/hero/DepartmentCat.jsx
@@ -11,7 +11,7 @@ const DepartmentCat = () => {
     const [showLinks, setShowLinks] = useState(false)
     return (
         <div className='mb-[50px]'>
-            <button onClick={() => setShowLinks(!showLinks)} className='bg-primary text-white p-2 h-12 font-bold w-[250px] flex gap-2 items-center justify-around relative'>
+            <button onClick={() => setShowLinks(!showLinks)} className='bg-primary text-white p-2 h-12 font-bold w-[250px] flex gap-2 items-center justify-around relative z-[1000]'>
                 <span className='flex gap-2'>
                     <IoMenu size={22} />
                     All departments
@@ -21,10 +21,10 @@ const DepartmentCat = () => {
             <AnimatePresence>
                 {showLinks &&
                     <motion.div
-                        initial={{ opacity: 0, y: 50 }} 
+                        initial={{ opacity: 0, y: -50 }} 
                         animate={{ opacity: 1, y: '0' }} // Set final position
-                        exit={{ opacity: 0, y: 50 }} // Animate out upward
-                        className='absolute z-[1000] bg-white w-[250px] flex flex-col gap-3 pl-6 py-4 border-[1px] transition-all duration-500 ease-in-out'>
+                        exit={{ opacity: 0, y: -50 }} // Animate out upward
+                        className='absolute z-[100] bg-white w-[250px] flex flex-col gap-3 pl-6 py-4 border-[1px] transition-all duration-500 ease-in-out '>
                         {Categories.map((item) => (
                             <Link key={item} href={`/`}>{item}</Link>
                         ))}
